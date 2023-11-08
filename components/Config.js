af19c706553cefdc4793b924a26911faadbf00c1
ergosha <112494979+ergosha@users.noreply.github.com>
2023-11-08 15:48:09 +0200

    Update Config.js

--- components/Config.js
+++ components/Config.js
@@ -3,7 +3,12 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  //add the correct configurations
+  apiKey: "AIzaSyD_BH98i8BkdhxUn_xQO6ZGU7XYJWPLkO4",
+  authDomain: "football-b58a5.firebaseapp.com",
+  projectId: "football-b58a5",
+  storageBucket: "football-b58a5.appspot.com",
+  messagingSenderId: "492336545948",
+  appId: "1:492336545948:web:a76aae4a3db54aaccfad59"
 }
 
 if (!firebase.apps.length) {
